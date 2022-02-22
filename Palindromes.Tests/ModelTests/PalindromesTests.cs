12abb82cfa81f253fd23928e48f8fd4c520d6c1e
Ryan Ashby <rjapsu160@gmail.com>
2022-02-22 13:21:15 -0800

    first and second test, comparing arrays, pass
    Co-authored-by: Will W. <wjwat@onionslice.org>

--- Palindromes.Tests/ModelTests/PalindromesTests.cs
+++ Palindromes.Tests/ModelTests/PalindromesTests.cs
@@ -7,10 +7,17 @@ namespace Palindromes.TestTools
   public class PalindromesTests
   {
     [TestMethod]
-    public void IsPalindrome_CheckReverseLetters_True()
+    public void IsPalindrome_CheckReverseLetters_False()
     {
       Palindrome newPalindrome = new Palindrome();
       Assert.AreEqual(false, newPalindrome.IsPalindrome("Bacon"));
+    }
+
+    [TestMethod]
+    public void IsPalindrome_CheckReverseLetters_True()
+    {
+      Palindrome newPalindrome = new Palindrome();
+      Assert.AreEqual(true, newPalindrome.IsPalindrome("Racecar"));
     }
   }
 }
