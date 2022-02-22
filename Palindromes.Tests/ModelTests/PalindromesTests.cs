84765d180649110e93f5b0e84319f500fbfcc6dc
Ryan Ashby <rjapsu160@gmail.com>
2022-02-22 13:48:30 -0800

    add second palindrome testing method/ PASS
    Co-authored-by: Will W. <wjwat@onionslice.org>

--- Palindromes.Tests/ModelTests/PalindromesTests.cs
+++ Palindromes.Tests/ModelTests/PalindromesTests.cs
@@ -19,5 +19,20 @@ namespace Palindromes.TestTools
       Palindrome newPalindrome = new Palindrome();
       Assert.AreEqual(true, newPalindrome.IsPalindrome("Racecar"));
     }
+
+    // isPalindrome2
+    [TestMethod]
+    public void IsPalindrome2_CheckReverseLetters_False()
+    {
+      Palindrome newPalindrome = new Palindrome();
+      Assert.AreEqual(false, newPalindrome.IsPalindrome2("Bacon"));
+    }
+
+    [TestMethod]
+    public void IsPalindrome2_CheckReverseLetters_True()
+    {
+      Palindrome newPalindrome = new Palindrome();
+      Assert.AreEqual(true, newPalindrome.IsPalindrome2("Racecar"));
+    }
   }
 }
