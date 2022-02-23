c2f825c60b2a3dd9be13ca94233ff716c6a6e1d7
Ryan Ashby <rjapsu160@gmail.com>
2022-02-22 16:13:53 -0800

    push all changes to github
    Co-authored-by: Will W. <wjwat@onionslice.org>

--- Palindromes/Models/Palindromes.cs
+++ Palindromes/Models/Palindromes.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 namespace Palindromes
 {
 
@@ -5,6 +8,37 @@ namespace Palindromes
   {
     public bool IsPalindrome(string input)
     {
+      char[] myArray = input.ToUpper().ToCharArray();
+      char[] reverseArray = new char[myArray.Length];
+      Array.Copy(myArray, reverseArray, myArray.Length);
+      Array.Reverse(reverseArray);
+      
+      // if (Enumerable.SequenceEqual(myArray, reverseArray)) --- One Way
+      if (myArray.SequenceEqual(reverseArray)) // --- Second Way
+      {
+        return true;
+      }
+      return false;
+    }
+    public bool IsPalindrome2(string input)
+    {
+      string test = input.ToUpper();
+      for (int i = 0, j = test.Length-1; i < test.Length; i++)
+      {
+        if (i > j-i)
+        {
+          return true;
+        }
+
+        if (test[i] == test[j-i])
+        {
+          continue;
+        }
+        else
+        {
+          return false;
+        }
+      }
       return true;
     }
   }
